dd300489f76e47e05ceaac230397757c1553260c
Renato Alfonso González Casanova Gallegos <renato.gonzalez@fi.unam.edu>
2024-02-20 19:18:01 -0600

    adf

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Woadfdrld!");
+Console.WriteLine("Hello, Woadfdasdrld!");
 Console.WriteLine("Hello, Wodddrld!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Worl55555555555d!");
